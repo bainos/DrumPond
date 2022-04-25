5beee1b0be8d90293f3ec53ae4a97683612b55fa
Jacopo Binosi aka bainos <b4inoz@gmail.com>
2022-04-25 08:09:54 +0200

    first steps

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DrumPond
 Lilypond for drummers
+
+# Credits
+https://gist.github.com/claymcleod/b670285f334acd56ad1c
--- drumpond_nc.py
+++ drumpond_nc.py
@@ -6,6 +6,10 @@ def draw_menu(stdscr):
     cursor_x = 0
     cursor_y = 0
 
+    command_mode = False
+    command = ""
+    statusbarstr = "Press 'q' to exit | {} | Pos: {}, {}".format(command, cursor_x, cursor_y)
+
     # Clear and refresh the screen for a blank canvas
     stdscr.clear()
     stdscr.refresh()
@@ -17,20 +21,44 @@ def draw_menu(stdscr):
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
     # Loop where k is the last character pressed
-    while (k != ord('q')):
+    while (k != ord('!')):
 
         # Initialization
         stdscr.clear()
         height, width = stdscr.getmaxyx()
 
-        if k == curses.KEY_DOWN:
-            cursor_y = cursor_y + 1
-        elif k == curses.KEY_UP:
-            cursor_y = cursor_y - 1
-        elif k == curses.KEY_RIGHT:
-            cursor_x = cursor_x + 1
-        elif k == curses.KEY_LEFT:
-            cursor_x = cursor_x - 1
+        if command_mode:
+            if k == 10:
+                if command == ":q":
+                    break
+                cursor_x = old_cursor_x
+                cursor_y = old_cursor_y
+                command_mode = False
+            else:
+                command = command+chr(k)
+                cursor_x = cursor_x+1
+        else:
+            if k == curses.KEY_DOWN:
+                cursor_y = cursor_y + 1
+            elif k == curses.KEY_UP:
+                cursor_y = cursor_y - 1
+            elif k == curses.KEY_RIGHT:
+                cursor_x = cursor_x + 1
+            elif k == curses.KEY_LEFT:
+                cursor_x = cursor_x - 1
+            elif k == 27: # ESC
+                stdscr.nodelay(True)
+                n = stdscr.getch()
+                if n == -1:
+                    command_mode = True
+                    command = ""
+                    statusbarstr = ""
+                    old_cursor_x = cursor_x
+                    old_cursor_y = cursor_y
+                    cursor_x = 0
+                    cursor_y = height-1
+                # Return to delay
+                stdscr.nodelay(False)
 
         cursor_x = max(0, cursor_x)
         cursor_x = min(width-1, cursor_x)
@@ -41,8 +69,11 @@ def draw_menu(stdscr):
         # Declaration of strings
         title = "Curses example"[:width-1]
         subtitle = "Written by Clay McLeod"[:width-1]
-        keystr = "Last key pressed: {}".format(k)[:width-1]
-        statusbarstr = "Press 'q' to exit | STATUS BAR | Pos: {}, {}".format(cursor_x, cursor_y)
+        keystr = "Last key pressed: {} {}".format(k,chr(k))[:width-1]
+        if command_mode:
+            statusbarstr = command
+        else:
+            statusbarstr = "Press 'q' to exit | {} | Pos: {}, {}".format(command, cursor_x, cursor_y)
         if k == 0:
             keystr = "No key press detected..."[:width-1]
 
@@ -80,7 +111,7 @@ def draw_menu(stdscr):
         stdscr.move(cursor_y, cursor_x)
 
         # Refresh the screen
-        stdscr.refresh()
+        #stdscr.refresh()
 
         # Wait for next input
         k = stdscr.getch()
